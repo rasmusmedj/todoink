e81e2abf31bbd1a31d8bbce1020b56af1abe994b
Rasmus Jensen <rje@Danpilot.dk>
2024-03-25 10:23:56 +0100

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+// import 'package:todoink/ui_updates_demo.dart';
+import 'package:todoink/keys/keys.dart';
+
+void main() {
+  runApp(const App());
+}
+
+class App extends StatelessWidget {
+  const App({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(useMaterial3: true),
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Flutter Internals'),
+        ),
+        body: const Keys(),
+      ),
+    );
+  }
+}
